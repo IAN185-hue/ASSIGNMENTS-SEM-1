54bef98947eeacbebdbfd77b8d4023a7b953e68b
Wanjiku Ian <ianomwangi275@gmail.com>
2024-10-18 16:08:01 +0300

    Add files via upload

---
+++ 1D ARRAY.c
@@ -0,0 +1,18 @@
+/* Name: WANJIKU IAN MACHARIA
+ * REGNO: CT101/G/23180/24
+ * A OME DIMENTIONAL ARRAY IN C LANGUAGE
+ */
+
+
+#include<stdio.h>
+// main function for scanf(). and printf() function
+int main() {
+    int i;
+    int results[6]={30,50,60,70,60,57};
+
+//A function to print the array
+    for (int i =0; i<6;i++) {
+        printf("%d\n ",results[i]);
+    }
+    return 0;
+    }
